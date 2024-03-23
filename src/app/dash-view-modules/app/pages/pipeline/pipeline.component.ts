1a4cfed110b877d6e69a8b13a8b636fb81d8415b
JuCoppin <julien.coppin@bstorm.be>
2024-03-23 09:35:13 +0100

    Pipeline view

--- src/app/dash-view-modules/app/pages/pipeline/pipeline.component.ts
+++ src/app/dash-view-modules/app/pages/pipeline/pipeline.component.ts
@@ -1,8 +1,22 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  ElementRef,
+  inject,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+  ViewEncapsulation,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IPipelineReadDTO, ITestClassReadDTO } from '@dash-view-common';
-import { CoreFormBuilder, TranslateService } from '@dash-view-core';
+import { CoreFormBuilder, LoadingContexts, LoadingService, TranslateService } from '@dash-view-core';
+import { Scroller } from 'primeng/scroller';
 import { PipelineService } from '../../services/pipeline.service';
 
 @Component({
@@ -10,16 +24,28 @@ import { PipelineService } from '../../services/pipeline.service';
   templateUrl: './pipeline.component.html',
   styleUrl: './pipeline.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class PipelineComponent implements OnInit {
+export class PipelineComponent implements OnInit, AfterViewInit {
+  @ViewChild('container') container!: ElementRef;
+  @ViewChild('sc') sc!: Scroller;
+  @ViewChildren('scrollTarget') scrollTargets!: QueryList<ElementRef>;
+
   repositoryID!: number;
   id!: number;
 
+  currentTestClassID!: number;
+
   item!: IPipelineReadDTO;
+  scrollerItems!: { id: number, name: string }[];
 
   sourceCodeMap: Map<number, string[]>;
   highlightedRowMap: Map<number, number[]>;
   checkboxRowMap: Map<number, number[]>;
+
+  maxCodeHeight!: string;
+
+  LoadingContexts = LoadingContexts;
 
   private destroyRef = inject(DestroyRef);
 
@@ -30,13 +56,25 @@ export class PipelineComponent implements OnInit {
     private readonly router: Router,
     private readonly fb: CoreFormBuilder,
     private readonly cd: ChangeDetectorRef,
+    private readonly loadingService: LoadingService,
   ) {
     this.sourceCodeMap = new Map<number, string[]>();
     this.highlightedRowMap = new Map<number, number[]>();
     this.checkboxRowMap = new Map<number, number[]>();
   }
 
+  checkboxChanged(event: any, row: number) {
+    // Handle checkbox change
+    console.log(`Checkbox at row ${row} changed to ${event.target.checked}`);
+  }
+
+  debug(testClass: ITestClassReadDTO) {
+    console.log(testClass);
+  }
+
   ngOnInit(): void {
+    this.maxCodeHeight = '100vh';
+
     this.activatedRoute.paramMap.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(s => {
       if (!s.get('repositoryID')) {
         return this.router.navigate(['/repositories']);
@@ -62,7 +100,18 @@ export class PipelineComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit(): void {
+    this.maxCodeHeight = `${this.container.nativeElement.offsetHeight}px`;
+    this.cd.detectChanges();
+
+    this.scrollTargets.changes.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
+    });
+  }
+
   private _load(): void {
+    this.loadingService.loadingCustom = true;
+    this.cd.detectChanges();
+
     this.sourceCodeMap.clear();
     this.highlightedRowMap.clear();
     this.checkboxRowMap.clear();
@@ -70,8 +119,11 @@ export class PipelineComponent implements OnInit {
     this.pipelineService.get({ repositoryID: this.repositoryID, id: this.id }).subscribe({
       next: s => {
         this.item = s;
+        this.scrollerItems = [];
 
         for (const testClass of this.item.testClasses) {
+          this.scrollerItems.push({ id: testClass.id, name: testClass.name });
+
           this.sourceCodeMap.set(testClass.id, testClass.sourceCode.split('\n'));
           this.highlightedRowMap.set(testClass.id, []);
           this.checkboxRowMap.set(testClass.id, []);
@@ -85,6 +137,8 @@ export class PipelineComponent implements OnInit {
           }
         }
 
+        this.loadingService.loadingCustom = false;
+
         this.cd.detectChanges();
       },
       error: () => {
@@ -93,12 +147,13 @@ export class PipelineComponent implements OnInit {
     })
   }
 
-  checkboxChanged(event: any, row: number) {
-    // Handle checkbox change
-    console.log(`Checkbox at row ${row} changed to ${event.target.checked}`);
-  }
+  updateScroller(i: number) {
+    this.currentTestClassID = this.item.testClasses[i].id;
 
-  debug(testClass: ITestClassReadDTO) {
-    console.log(testClass);
+    // console.log(this.currentTestClassID);
+
+    if (this.sc) {
+      this.sc.scrollToIndex(i, 'smooth');
+    }
   }
 }
